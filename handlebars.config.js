deb429a489451756f5884708d6cd2e98a5c107ae
EkaterinaBelonosova <katerina2594@mail.ru>
2022-08-19 17:41:25 +0700

    add data json

--- handlebars.config.js
+++ handlebars.config.js
@@ -3,4 +3,5 @@ const path = require('path');
 module.exports = {
   layouts: path.join(__dirname, 'src/pages'),
   partials: path.join(__dirname, 'src/components'),
+  data:  path.join(__dirname, 'src/data'),
 };
